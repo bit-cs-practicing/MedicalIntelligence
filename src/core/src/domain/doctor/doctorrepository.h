9cbfe3585ed94300850e64ff78afcabfdf7527b7
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2025-08-29 09:51:38 +0800

    feat(core/domain): add method in DoctorRepository

--- src/core/src/domain/doctor/doctorrepository.h
+++ src/core/src/domain/doctor/doctorrepository.h
@@ -11,6 +11,7 @@ public:
     virtual void save(const Doctor& doctor) = 0;
     virtual std::optional<Doctor> getById(const Id& id) const = 0;
     virtual std::optional<Doctor> getByIdCard(const IdCard& idCard) const = 0;
+    virtual std::optional<Doctor> getAll() const = 0;
 };
 
 #endif // DOCTORREPOSITORY_H
