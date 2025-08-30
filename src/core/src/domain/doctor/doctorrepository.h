bf927d90e9701ed9437f99a9050419409bf3ddae
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2025-08-30 09:02:11 +0800

    feat(core/app): add application service for Doctor

--- src/core/src/domain/doctor/doctorrepository.h
+++ src/core/src/domain/doctor/doctorrepository.h
@@ -3,6 +3,7 @@
 
 #include "doctor.h"
 #include <optional>
+#include <QList>
 
 class DoctorRepository {
 public:
@@ -11,7 +12,7 @@ public:
     virtual void save(const Doctor& doctor) = 0;
     virtual std::optional<Doctor> getById(const Id& id) const = 0;
     virtual std::optional<Doctor> getByIdCard(const IdCard& idCard) const = 0;
-    virtual std::optional<Doctor> getAll() const = 0;
+    virtual QList<Doctor> getAll() const = 0;
 };
 
 #endif // DOCTORREPOSITORY_H
