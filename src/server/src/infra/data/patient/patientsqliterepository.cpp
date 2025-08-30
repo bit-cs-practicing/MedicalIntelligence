ea235b45d8a28cec6521f4fdeb0dfb059df26aa7
keketcl <376023052@qq.com>
2025-08-30 11:29:44 +0800

    refactor(server/infra): extract createConnection and getPatientFromQuery from PatientSQLiteRepository

--- src/server/src/infra/data/patient/patientsqliterepository.cpp
+++ src/server/src/infra/data/patient/patientsqliterepository.cpp
@@ -1,53 +1,16 @@
 #include "patientsqliterepository.h"
+#include "../util/databaseoperator/databaseoperator.h"
 
-#include <QUuid>
 #include <QVariant>
 #include <exception>
 #include <QtSql/QSqlQuery>
 #include <QDebug>
 
 PatientSQLiteRepository::PatientSQLiteRepository(const QString& path) {
-    db = QSqlDatabase::addDatabase("QSQLITE", "Patient" + QUuid::createUuid().toString());
-    db.setDatabaseName(path);
-//    qDebug() << "set over";
-    if (!db.open()) {
-        QString msg = QString("Fail to open the database from %1").arg(path);
-        throw std::runtime_error(msg.toStdString());
-    }
-//    qDebug() << "over";
+    DatabaseOperator::createConnection(&db, "Patient", path);
 }
 
-Patient getPatientFromQuery(const QSqlQuery& query) {
-//    qDebug() << "1";
-    Name name(query.value(1).toString());
-//    qDebug() << "2";
-    IdCard idCard(query.value(2).toString());
-//    qDebug() << "3";
-    Password password(query.value(3).toString());
-//    qDebug() << "4";
-    Gender gender(query.value(4).toString());
-//    qDebug() << "5";
-    Phone phone(query.value(5).toString());
-//    qDebug() << "6";
-    Patient patient(name, idCard, gender, phone, password);
-//    qDebug() << "7";
-
-    QVariant birthday = query.value(6);
-//    qDebug() << "8";
-    if (!birthday.isNull() && birthday.toString() != "") patient.setBirthday(Birthday(birthday.toDate()));
-//    qDebug() << "9";
-    QVariant email = query.value(7);
-//    qDebug() << "10";
-    if (!email.isNull() && email.toString() != "") patient.setEmail(Email(email.toString()));
-//    qDebug() << "11";
-    QVariant emergencyContact = query.value(8);
-//    qDebug() << "12";
-    if (!emergencyContact.isNull() && emergencyContact.toString() != "") patient.setEmergencyContact(Phone(emergencyContact.toString()));
-//    qDebug() << "13";
-    return patient;
-}
-
-//void print(const std::optional<Patient>&);
+PatientSQLiteRepository::~PatientSQLiteRepository() {db.close();}
 
 void PatientSQLiteRepository::save(const Patient &patient) {
     QSqlQuery query(db);
@@ -89,7 +52,7 @@ std::optional<Patient> PatientSQLiteRepository::getById(const Id &id) const {
 //    else qDebug() << "fail";
 //    qDebug() << "!";
     if (!query.next()) return std::nullopt;
-    return getPatientFromQuery(query);
+    return DatabaseOperator::getPatientFromQuery(query);
 }
 
 std::optional<Patient> PatientSQLiteRepository::getByIdCard(const IdCard &idCard) const {
@@ -98,9 +61,5 @@ std::optional<Patient> PatientSQLiteRepository::getByIdCard(const IdCard &idCard
     query.bindValue(":idCard", idCard.getValue());
     query.exec();
     if (!query.next()) return std::nullopt;
-    return getPatientFromQuery(query);
-}
-
-PatientSQLiteRepository::~PatientSQLiteRepository() {
-    db.close();
+    return DatabaseOperator::getPatientFromQuery(query);
 }
