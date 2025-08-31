0f9740585e2e2a1081aaa4556a5c227fd7a96230
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2025-08-31 09:27:25 +0800

    feat(server/infra): add controller interfaces

---
+++ src/server/src/infra/rpcserver/rpcdispatcher.h
@@ -0,0 +1,25 @@
+#ifndef RPCDISPATCHER_H
+#define RPCDISPATCHER_H
+
+#include <memory>
+
+#include <QMap>
+
+#include "infra/protocol/request.h"
+#include "infra/protocol/response.h"
+
+#include "rpccontroller.h"
+
+class RpcDispatcher : public RpcController {
+public:
+    RpcDispatcher();
+
+    void add(QString endpoint, std::unique_ptr<RpcController> controller);
+protected:
+    virtual Response processImpl(const Request& request) override;
+private:
+    QMap<QString, std::unique_ptr<RpcController>> controllers;
+
+};
+
+#endif // RPCDISPATCHER_H
