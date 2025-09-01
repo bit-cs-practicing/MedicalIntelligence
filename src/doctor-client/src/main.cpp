537af42c2883d01bf782492b7dca457efeb7ebb8
Xiangyu Wang <wwxyy2005@126.com>
2025-09-02 00:18:35 +0800

    feat(doctor-client/view): add doctor client

--- src/doctor-client/src/main.cpp
+++ src/doctor-client/src/main.cpp
@@ -1,10 +1,28 @@
-#include "mainwindow.h"
+#include "doctorlogin.h"
+#include "doctor.h"
 
 #include <QApplication>
-
-int main(int argc, char *argv[]) {
+#include <QSettings>
+int main(int argc, char *argv[])
+{
     QApplication a(argc, argv);
-    MainWindow w;
+    QString srcPath = QApplication::applicationDirPath() + "/setting.ini";
+    QSettings settings(srcPath, QSettings::IniFormat);
+    settings.beginGroup("doctor");
+    if (!settings.contains("ip")) {
+        settings.setValue("ip", "127.0.0.1");
+    }
+    if (!settings.contains("port")) {
+        settings.setValue("port", 8080);
+    }
+    QString address = settings.value("ip").toString();
+    int port = settings.value("port").toInt();
+    settings.endGroup();
+
+    RpcClient Sender(QHostAddress(address), port);
+    CredentialManager doctorCredential;
+    DoctorLogin w(nullptr, &Sender, &doctorCredential);
+    // QObject::connect(&w, &DoctorLogin::loginSucceed, &d, &Doctor::onLoginSuccess);
     w.show();
     return a.exec();
 }
