8cbb45629936166a9a159e46f3311ba8b893ad3f
keketcl <376023052@qq.com>
2025-09-02 16:51:28 +0800

    fix(patient/register): sinicize gender

--- src/patient-client/src/patientregister.cpp
+++ src/patient-client/src/patientregister.cpp
@@ -57,56 +57,59 @@ bool PatientRegister::checkEmail(){
     return std::regex_match(ui->email->text().toStdString(), emailRegex);
 }
 
-
+QString transferGenderFromZHToEN(const QString& ZHContext) {
+    if (ZHContext == QString("男")) {
+        return "male";
+    } else {
+        return "female";
+    }
+}
 
 void PatientRegister::on_register_2_clicked()
 {
-    if(!checkName()) {
+    if (!checkName()) {
         QMessageBox::warning(this, "警告！", "请输入真实姓名！");
         return;
     }
-    else if(!checkIdCard()) {
+    else if (!checkIdCard()) {
         QMessageBox::warning(this, "警告！", "身份证号错误！");
         return;
     }
-    else if(!checkEmail()) {
+    else if (!checkEmail()) {
         QMessageBox::warning(this, "警告！", "请输入合法的邮箱！");
         return;
     }
-    else if(!checkPassword()) {
+    else if (!checkPassword()) {
         QMessageBox::warning(this, "警告！", "请输入7到20位密码，密码只应包含数字、大小写字母！");
         return;
     }
-    else if(ui->password->text() != ui->checkpass->text()){
+    else if (ui->password->text() != ui->checkpass->text()){
         QMessageBox::warning(this, "警告！", "两次输入的密码不一致！");
         return;
     }
-    else if(!checkPhone(ui->phone->text())) {
+    else if (!checkPhone(ui->phone->text())) {
         QMessageBox::warning(this, "警告！", "请输入合法的电话号码！");
         return;
     }
-    else if(!checkPhone(ui->emgcy->text())) {
+    else if (!checkPhone(ui->emgcy->text())) {
         QMessageBox::warning(this, "警告！", "请输入合法的紧急联系人电话！");
         return;
     }
     QJsonObject registerData;
     registerData["name"] = ui->name->text();
     registerData["password"] = ui->password->text();
-    registerData["gender"] = ui->gender->currentText();
-//    qDebug() << registerData["gender"] << "\n";
+    registerData["gender"] = transferGenderFromZHToEN(ui->gender->currentText());
     registerData["idCard"] = ui->idCard->text();
     registerData["phone"] = ui->phone->text();
     registerData["birthday"] = ui->birthday->date().toString("yyyy-MM-dd");
-//    qDebug() << registerData["birthday"] << "\n";
     registerData["email"] = ui->email->text();
     registerData["emergencyContact"] = ui->emgcy->text();
     Response result = requestSender->rpc(Request("patient.register", patientCredential->get(), registerData));
     qDebug() << result.data << "\n";
-    if(!result.success) {
+    if (!result.success) {
         QMessageBox::warning(this, "警告！", "您已注册过账号！");
         return;
-    }
-    else{
+    } else {
         QMessageBox::information(this, "恭喜!", "注册成功！将在3秒后返回登录界面。");
         QThread::sleep(3);
         loginBoard -> show();
